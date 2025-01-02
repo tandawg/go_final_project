9f8ffec6318dc1bfc78934c0bbfb88e907f7c52d
Tatiana Fedorova <tandawg@yandex.ru>
2025-01-02 21:45:30 +0300

    Обновлены файлы тестов (sqlite3->sqlite)

--- tests/settings.go
+++ tests/settings.go
@@ -1,7 +1,7 @@
 package tests
 
 var Port = 7540
-var DBFile = "../scheduler.db"
+var DBFile = "C:/Users/Tanya/Dev/go_final_project/scheduler.db"
 var FullNextDate = false
 var Search = false
 var Token = ``
